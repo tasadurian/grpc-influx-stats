c484e1967c99190fa2dfc25de1c60d5318966b8b
Thomas Asadurian <Tasadurian@gmail.com>
2017-11-25 16:42:26 -0800

    added test to make sure UnaryServerInterceptor does not panic

---
+++ stats.go
@@ -0,0 +1,71 @@
+package stats
+
+import (
+	"time"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+
+	client "github.com/influxdata/influxdb/client/v2"
+)
+
+// InfluxOptions ...
+type InfluxOptions struct {
+	Measurement string
+	Database    string
+	Tags        map[string]string
+	Fields      map[string]interface{}
+}
+
+// NewInfluxClient creates a new InfluxDB client
+func NewInfluxClient(address string) (client.Client, error) {
+	addr := "127.0.0.1:8089"
+	if address != "" {
+		addr = address
+	}
+
+	influx, err := client.NewUDPClient(client.UDPConfig{Addr: addr})
+	return influx, err
+}
+
+// WriteToInflux ....
+// tags = things easy to index on
+// fields - things that change a lot - ie. request time
+func WriteToInflux(opts InfluxOptions, InfluxClient client.Client) error {
+	bp, err := client.NewBatchPoints(client.BatchPointsConfig{Database: opts.Database})
+	if err != nil {
+		return err
+	}
+	pt, err := client.NewPoint(opts.Measurement, opts.Tags, opts.Fields, time.Now())
+	if err != nil {
+		return err
+	}
+	bp.AddPoint(pt)
+	err = InfluxClient.Write(bp)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// UnaryServerInterceptor is a grpc middleware that logs latency
+// to influx db.
+func UnaryServerInterceptor(c client.Client, opts InfluxOptions) grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		startTime := time.Now()
+		resp, err := handler(ctx, req)
+		latency := time.Now().Sub(startTime)
+
+		tags := map[string]string{
+			"method": info.FullMethod,
+		}
+		fields := map[string]interface{}{
+			"latency": latency.Seconds() * 1000,
+		}
+		opts.Tags = tags
+		opts.Fields = fields
+		WriteToInflux(opts, c)
+
+		return resp, err
+	}
+}
---
+++ stats_test.go
@@ -0,0 +1,115 @@
+package stats
+
+import (
+	"net"
+	"testing"
+	"time"
+
+	pb_testproto "github.com/grpc-ecosystem/go-grpc-middleware/testing/testproto"
+	client "github.com/influxdata/influxdb/client/v2"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+)
+
+const (
+	pingDefaultValue   = "Hello World."
+	countListResponses = 20
+)
+
+func TestServerInterceptorSuite(t *testing.T) {
+	suite.Run(t, &ServerInterceptorTestSuite{})
+}
+
+type ServerInterceptorTestSuite struct {
+	suite.Suite
+
+	serverListener net.Listener
+	server         *grpc.Server
+	clientConn     *grpc.ClientConn
+	testClient     pb_testproto.TestServiceClient
+	ctx            context.Context
+	c              client.Client
+}
+
+func (s *ServerInterceptorTestSuite) SetupSuite() {
+	var err error
+
+	s.c, err = NewInfluxClient("")
+
+	s.serverListener, err = net.Listen("tcp", "127.0.0.1:0")
+	require.NoError(s.T(), err, "must be able to allocate a port for serverListener")
+
+	// This is the point where we hook up the interceptor
+	s.server = grpc.NewServer(
+		//grpc.StreamInterceptor(StreamServerInterceptor),
+		grpc.UnaryInterceptor(UnaryServerInterceptor(s.c, InfluxOptions{})),
+	)
+	pb_testproto.RegisterTestServiceServer(s.server, &testService{t: s.T()})
+
+	go func() {
+		s.server.Serve(s.serverListener)
+	}()
+
+	s.clientConn, err = grpc.Dial(s.serverListener.Addr().String(), grpc.WithInsecure(), grpc.WithBlock(), grpc.WithTimeout(2*time.Second))
+	require.NoError(s.T(), err, "must not error on client Dial")
+	s.testClient = pb_testproto.NewTestServiceClient(s.clientConn)
+}
+
+func (s *ServerInterceptorTestSuite) SetupTest() {
+	// Make all RPC calls last at most 2 sec, meaning all async issues or deadlock will not kill tests.
+	s.ctx, _ = context.WithTimeout(context.TODO(), 2*time.Second)
+}
+
+func (s *ServerInterceptorTestSuite) TearDownSuite() {
+	if s.serverListener != nil {
+		s.server.Stop()
+		s.T().Logf("stopped grpc.Server at: %v", s.serverListener.Addr().String())
+		s.serverListener.Close()
+
+	}
+	if s.clientConn != nil {
+		s.clientConn.Close()
+	}
+}
+
+func (s *ServerInterceptorTestSuite) TestUnaryIncrementsStarted() {
+	s.testClient.PingEmpty(s.ctx, &pb_testproto.Empty{})
+
+	s.testClient.PingError(s.ctx, &pb_testproto.PingRequest{ErrorCodeReturned: uint32(codes.Unavailable)})
+}
+
+type testService struct {
+	t *testing.T
+}
+
+func (s *testService) PingEmpty(ctx context.Context, _ *pb_testproto.Empty) (*pb_testproto.PingResponse, error) {
+	return &pb_testproto.PingResponse{Value: pingDefaultValue, Counter: 42}, nil
+}
+
+func (s *testService) Ping(ctx context.Context, ping *pb_testproto.PingRequest) (*pb_testproto.PingResponse, error) {
+	// Send user trailers and headers.
+	return &pb_testproto.PingResponse{Value: ping.Value, Counter: 42}, nil
+}
+
+func (s *testService) PingError(ctx context.Context, ping *pb_testproto.PingRequest) (*pb_testproto.Empty, error) {
+	code := codes.Code(ping.ErrorCodeReturned)
+	return nil, grpc.Errorf(code, "Userspace error.")
+}
+
+func (s *testService) PingStream(ping pb_testproto.TestService_PingStreamServer) error {
+	return nil
+}
+
+func (s *testService) PingList(ping *pb_testproto.PingRequest, stream pb_testproto.TestService_PingListServer) error {
+	if ping.ErrorCodeReturned != 0 {
+		return grpc.Errorf(codes.Code(ping.ErrorCodeReturned), "foobar")
+	}
+	// Send user trailers and headers.
+	for i := 0; i < countListResponses; i++ {
+		stream.Send(&pb_testproto.PingResponse{Value: ping.Value, Counter: int32(i)})
+	}
+	return nil
+}
